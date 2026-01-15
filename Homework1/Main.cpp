441f8b1eb8c2ef092c1223c936a3ea1e19264a6f
Nikolay Paunov <nip@milestone.dk>
2026-01-15 08:52:12 +0200

    Initialize task 3

--- Homework1/Main.cpp
+++ Homework1/Main.cpp
@@ -1,8 +1,10 @@
 #include "Task1.h"
 #include "Task2.h"
+#include "Task3.h"
 
 int main() {
     //Task1();
-    Task2();
+    //Task2();
+    Task3();
     return 0;
 }
---
+++ Homework1/Task3.cpp
@@ -0,0 +1,8 @@
+#include "Task2.h"
+#include <iostream>
+
+using namespace std;
+
+void Task3() {
+	cout << "Task3 is not implemented yet." << endl;
+}
